c13b591602252ecc2ccc81cdce004313142b2149
ktrajasekar <kt.rajasekar@gmail.com>
2016-02-10 13:07:18 +0530

    SPA Angularjs

---
+++ app/app.js
@@ -0,0 +1,32 @@
+var applicationSpa = angular.module('applicationSpa', ['ngRoute']);
+
+// configure our routes
+applicationSpa.config(function($routeProvider) {
+
+    $routeProvider
+    .when('/',
+    {
+        templateUrl:'pages/home.html',
+        controller:'MainController'
+    })
+      .when('/faq',
+    {
+        templateUrl:'pages/faq.html',
+          controller:'aboutController'
+    })
+    .otherwise({
+            redirectTo: '/'
+    });
+});
+
+    // create the controller and inject Angular's $scope
+    applicationSpa.controller('MainController', function($scope) { 
+        // create a message to display in our view
+        $scope.message = 'Welcome to home page';
+    });
+
+ // create the controller and inject Angular's $scope
+    applicationSpa.controller('aboutController', function($scope) { 
+        // create a message to display in our view
+        $scope.message = 'Welcome to About us page';
+    });
